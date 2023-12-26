633bdec498612dc83291fd2f83e82baf7ea3162d
aaronmergui <merguiaaron@gmail.com>
2023-12-26 19:42:09 +0200

    hello

--- http_server.py
+++ http_server.py
@@ -5,12 +5,25 @@ app=Flask(__name__)
 
 @app.route('/sum')
 def addition():
-    a=request.args
-    a=request.args.get('a', default=0,type=float)
-    b=request.args.get('b', default=0,type=float)
-    c=a+b
-    message = f"The result of {a}+{b} is: {c}"
-    return '''<h2>{}</h2>'''.format(message)
+    try:
+        a=float(request.args.get('a'))
+        b=float(request.args.get('b'))
+        c = a + b
+        message = f"The result of {a}+{b} is: {c}"
+        return '''<h2>{}</h2>'''.format(message)
+    except:
+        return '''<h2>At least one of the parameters is not a number </h2>'''
+
+@app.route('/average')
+def average():
+    try:
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+        c = a + b
+        message = f"The average is: {c/2}"
+        return '''<h2>{}</h2>'''.format(message)
+    except:
+        return '''<h2>At least one of the parameters is not a number </h2>'''
 
 
 if __name__ == '__main__':
