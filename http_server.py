3d1f8269989a47e58e5f93aa253a86a8a362a5f3
aaronmergui <merguiaaron@gmail.com>
2023-12-26 20:32:33 +0200

    last one

--- http_server.py
+++ http_server.py
@@ -6,24 +6,27 @@ app=Flask(__name__)
 @app.route('/sum')
 def addition():
     try:
-        a=float(request.args.get('a'))
-        b=float(request.args.get('b'))
-        c = a + b
-        message = f"The result of {a}+{b} is: {c}"
+        numbers=list(map(float,request.args.values()))
+        s=lambda num:sum(num)
+        message = f"The result of sum is: {s(numbers)}"
         return '''<h2>{}</h2>'''.format(message)
     except:
-        return '''<h2>At least one of the parameters is not a number </h2>'''
+        return '''<h2>http error code bad request </h2>'''
 
 @app.route('/average')
 def average():
     try:
-        a = float(request.args.get('a'))
-        b = float(request.args.get('b'))
-        c = a + b
-        message = f"The average is: {c/2}"
-        return '''<h2>{}</h2>'''.format(message)
+        numbers = list(map(float, request.args.values()))
+        av=lambda num:sum(num)/len(num) if len(num)!=0 else 0
+        return '''<h2>The average is: {} </h2>'''.format(av(numbers))
+
+
     except:
-        return '''<h2>At least one of the parameters is not a number </h2>'''
+        return '''<h2>http error code bad request </h2>'''
+@app.errorhandler(404)
+def page_not_found(error):
+    return "this page doesn't exist",404
+
 
 
 if __name__ == '__main__':
